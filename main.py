54fd22fe636d83ad09cfacb6da6b21c016d8d25c
Harishankar Vashishtha <139641664+harishankar-bcindia@users.noreply.github.com>
2024-02-12 15:30:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root1():
+    return {"Message": "Hello Harishankar"}
+
+@app.post("/fruit/{fruit_name}")
+def read_root2():
+    return {"Message": f"Hello Harishankar, your favourite fruit is {fruit_name}"}
